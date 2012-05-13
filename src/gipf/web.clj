812d01bca67d9559fd409fcabb2aa63a2e050e84
Curtis Badke <curtis@badkid.ca>
2012-05-12 21:21:44 -0600

    Add Clojure adder example

--- src/gipf/web.clj
+++ src/gipf/web.clj
@@ -1,10 +1,46 @@
 (ns gipf.web
-  (:use [ring.adapter.jetty :only [run-jetty]]))
+  (:use [ring.adapter.jetty :only [run-jetty]])
+  (:use [ring.middleware.params :only [wrap-params]])
+  (:use compojure.core)
+  (:use hiccup.core)
+  (:use hiccup.page))
 
-(defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Gipf-Online"})
+(defn view-layout [& content]
+  (xhtml
+    (xhtml-tag "en"
+               [:head
+                [:meta {:http-equiv "Content-type"
+                        :content "text/html; charset=utf-8"}]
+                [:title "adder"]]
+               [:body content])))
+
+(defn view-input []
+  (view-layout
+    [:h2 "add two numbers"]
+    [:form {:method "post" :action "/"}
+     [:input.math {:type "text" :name "a"}] [:span.math " + "]
+     [:input.math {:type "text" :name "b"}] [:br]
+     [:input.action {:type "submit" :value "add"}]]))
+
+(defn view-output [a b sum]
+  (view-layout
+    [:h2 "two numbers added"]
+    [:p.math a " + " b " = " sum]
+    [:a.action {:href "/"} "add more numbers"]))
+
+(defn parse-input [a b]
+  [(read-string a) (read-string b)])
+
+(defroutes main-routes
+           (GET "/" []
+                (view-input))
+
+           (POST "/" [a b]
+                 (let [[a b] (parse-input a b)
+                       sum (+ a b)]
+                   (view-output a b sum))))
+
+(def app (wrap-params main-routes))
 
 (defn -main [port]
   (run-jetty app {:port (Integer. port)}))
