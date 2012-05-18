2c9330a77ec57710bdf6c7118fb591136275cec8
Curtis Badke <curtis@badkid.ca>
2012-05-17 21:01:32 -0600

    Make use statements more specific

--- src/web/views/tamsk.clj
+++ src/web/views/tamsk.clj
@@ -1,6 +1,6 @@
 (ns web.views.tamsk
   (:use [noir.core :only [defpartial defpage]]
-        [web.views.common]
+        [web.views.common :only [page-wrapper]]
         [hiccup.core]
         [hiccup.page-helpers :only [include-css include-js html5 link-to]]))
 
