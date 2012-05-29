9bbeac1e95d0e7b4497e51905445c3fcca34b3a5
Curtis Badke <curtis@badkid.ca>
2012-05-28 21:16:54 -0600

    Fix missed namespace change on previous reorg

--- src/gipf_online/web/views/yinsh.clj
+++ src/gipf_online/web/views/yinsh.clj
@@ -1,6 +1,6 @@
 (ns gipf-online.web.views.yinsh
   (:use [noir.core :only [defpartial defpage]]
-        [web.views.common :only [page-wrapper]]
+        [gipf-online.web.views.common :only [page-wrapper]]
         [hiccup.core]
         [hiccup.page-helpers :only [include-css include-js html5 link-to]]))
 
